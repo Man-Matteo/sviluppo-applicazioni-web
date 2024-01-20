ed4d61040051fdecc15df9c6a23137e5386280d4
Matte <matteo.mannai@outlook.it>
2024-01-20 20:39:32 +0100

    Remove unnecessary JavaScript files from search.php and show_profile.php

--- show_profile.php
+++ show_profile.php
@@ -4,8 +4,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/show_profile_style.css">
-        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-        <script src="logout.js"></script>
         <title>Profile</title>
     </head>
     <body>
