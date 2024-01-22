a443e240126c200cc5a0b3f35b41f6d409e8122b
Matte <matteo.mannai@outlook.it>
2024-01-22 10:49:56 +0100

    Fix user identification issue in add_product.php

--- functions/add_product.php
+++ functions/add_product.php
@@ -1,12 +1,11 @@
 <?php
-    //non posso controllare che l'utente sia loggato o meno perchè sennò quando aggiungo un prodotto al carrello da non loggato mi manda al login     
     require('functions.php');
     $conn = readWriteConnection();
 
     $productId = $_GET['productId'];
     $quantity = $_GET['quantity'];
-    //if user logged in use email as id, else use session id
-    $userEmail = IfLogged() ? $_SESSION['email'] : session_id();
+    $userEmail =  $_SESSION['email'];
+    //$userEmail = IfLogged() ? $_SESSION['email'] : session_id();
 
     updateOrAddCart($conn, $userEmail, $productId, $quantity);
 
